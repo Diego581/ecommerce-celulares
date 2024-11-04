e2132d8a8608de47fe2e4c4695ef09bad0174328
Diego <diego445.gonzalez@gmail.com>
2024-11-04 19:47:03 -0300

    fix boton iniciar sesion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
-// src/components/Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+    const { token, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout(); // Llama a la función logout del contexto
+        navigate('/login'); // Redirige a la página de login
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light w-100"> {/* Asegúrate de que el navbar ocupe el 100% del ancho */}
-            <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
+            <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Admin Panel</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav me-auto"> {/* Elementos alineados a la izquierda */}
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Dashboard</Link>
                         </li>
@@ -22,10 +31,16 @@ const Navbar = () => {
                             <Link className="nav-link" to="/equipos">Equipos</Link>
                         </li>
                     </ul>
-                    <ul className="navbar-nav"> {/* Elementos alineados a la derecha */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Iniciar Sesión</Link>
-                        </li>
+                    <ul className="navbar-nav">
+                        {token ? (
+                            <li className="nav-item">
+                                <button className="nav-link btn btn-link" onClick={handleLogout}>Cerrar Sesión</button>
+                            </li>
+                        ) : (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Iniciar Sesión</Link>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
