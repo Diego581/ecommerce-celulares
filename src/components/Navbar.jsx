3e00a528ca2167d2500b282b644bb513dc90fb7c
Diego <diego445.gonzalez@gmail.com>
2024-11-09 19:28:30 -0300

    fix navbar panel usuario-admin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    {role === 'usuario' ? 'User Panel' : 'Admin Panel'}
+                    {role === 'usuario' ? 'Panel de Usuario' : 'Panel de Admin'}
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -41,11 +41,11 @@ const Navbar = () => {
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/users">Usuarios</Link>
                                 </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/equipos">Equipos</Link>
-                                </li>
                             </>
                         )}
+                        <li className="nav-item">
+                                    <Link className="nav-link" to="/equipos">Equipos</Link>
+                        </li>
                     </ul>
                     <ul className="navbar-nav">
                         <li className="nav-item">
